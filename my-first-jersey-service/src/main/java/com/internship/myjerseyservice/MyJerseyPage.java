66f2edaac29cd5bade5a9934863fcc183f3a5bc6
Yenny Davidson <ydavidson2021@gmail.com>
2021-04-06 14:26:08 -0500

    First Jersey project

---
+++ my-first-jersey-service/src/main/java/com/internship/myjerseyservice/MyJerseyPage.java
@@ -0,0 +1,19 @@
+package com.internship.myjerseyservice;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/hello")
+public class MyJerseyPage {
+@GET
+@Produces(MediaType.TEXT_HTML)
+public String sayHtmlHello() {
+return "Hello from Jersey";
+}
+}
